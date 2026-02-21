9f2d89a444a37c03d542acccb9a57d6addb12da9
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-21 09:21:25 +0900

    Add test case for Resource::SQLite including C source, config with static linking for Windows, and updated MANIFEST.

--- t/lib/SPVM/TestCase/Resource/SQLite.c
+++ t/lib/SPVM/TestCase/Resource/SQLite.c
@@ -1,12 +1,20 @@
 #include "spvm_native.h"
 
-
+// Include sqlite3.h provided by Resource::SQLite
+#include "sqlite3.h"
 
 int32_t SPVM__TestCase__Resource__SQLite__test(SPVM_ENV* env, SPVM_VALUE* stack) {
   
-  stack[0].ival = 1;
+  // Get the SQLite library version number (e.g., 3045000)
+  int32_t version_number = sqlite3_libversion_number();
+  
+  // Check if the version number is valid (greater than 0)
+  if (version_number > 0) {
+    stack[0].ival = 1;
+  }
+  else {
+    stack[0].ival = 0;
+  }
   
   return 0;
 }
-
-
